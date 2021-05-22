6046b16b5bc5cceeac71ee4f6c44e263d4186b64
lab user <dev@tonimueller.org>
2021-05-22 13:13:51 +0000

    added new network

---
+++ vpc.tf
@@ -0,0 +1,18 @@
+module "nework" {
+
+  source  = "app.terraform.io/linux-python/network/google"
+  version = "2.5.0"
+  project_id    = var.project
+
+  network_name  = "db-net"
+
+
+  subnets = [
+    {
+      subnet_name   = "gaurav-subnet"
+      subnet_ip     = "10.100.10.0/24"
+      subnet_region = var.region
+    }
+  ]
+
+}
